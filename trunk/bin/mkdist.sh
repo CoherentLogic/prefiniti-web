748b5906cb436618332ff30bd77cead3091f9f32
chocolatejollis38@gmail.com <chocolatejollis38@gmail.com@c2d0bc50-b47c-30b3-8307-b90581f7d908>
2011-07-22 23:16:38 +0000

    commit

--- trunk/bin/mkdist.sh
+++ trunk/bin/mkdist.sh
@@ -21,8 +21,11 @@ echo "GETTING THE SETUP SCRIPT..."
 mv prefiniti-web/setup/${PLATFORM}/setup.sh .
 echo "NORMALIZING DISTRIBUTION..."
 rm -Rf setup
-echo "BUILDING ARCHIVE..."
-tar cvf prefiniti-${PLATFORM}.tar prefiniti-web/ setup.sh
+echo "BUILDING FILES ARCHIVE..."
+tar cvf prefiniti-${PLATFORM}-files.tar prefiniti-web/ > /dev/null
+gzip prefiniti-${PLATFORM}-files.tar
+echo "BUILDING FULL ARCHIVE..."
+tar cvf prefiniti-${PLATFORM}.tar prefiniti-${PLATFORM}-files.tar.gz setup.sh > /dev/null
 gzip prefiniti-${PLATFORM}.tar
 rm -Rf prefiniti-web
 echo "DONE."
